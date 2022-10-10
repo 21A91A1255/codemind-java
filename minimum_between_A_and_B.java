d45fa7b174b5974491e0fcaacc1696859b4727d3
21A91A1255 <97424886+21A91A1255@users.noreply.github.com>
2022-10-10 16:34:18 +0530

    import java.util.Scanner; class First{     public static void main(String args[])     {         int a,b,d,i,c=0,max=1000,p=0;         Scanner sc= new Scanner(System.in);         a=sc.nextInt();         int[] arr = new int[100];        for(i=0;i<a;i++)        {            arr[i]=sc.nextInt();        }        b=sc.nextInt();        d=sc.nextInt();        for(i=0;i<a;i++)        {         if(arr[i]>=b && arr[i]<=d)         {             p++;             if(arr[i]<max)             {                 max=arr[i];             }         }        }     if(p!=0)     {     System.out.print(max);     }     else     {        System.out.print("-1");     } } }

---
+++ minimum_between_A_and_B.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class First{
+    public static void main(String args[])
+    {
+        int a,b,d,i,c=0,max=1000,p=0;
+        Scanner sc= new Scanner(System.in);
+        a=sc.nextInt();
+        int[] arr = new int[100];
+       for(i=0;i<a;i++)
+       {
+           arr[i]=sc.nextInt();
+       }
+       b=sc.nextInt();
+       d=sc.nextInt();
+       for(i=0;i<a;i++)
+       {
+        if(arr[i]>=b && arr[i]<=d)
+        {
+            p++;
+            if(arr[i]<max)
+            {
+                max=arr[i];
+            }
+        }
+       }
+    if(p!=0)
+    {
+    System.out.print(max);
+    }
+    else
+    {
+       System.out.print("-1");
+    }
+}
+}
