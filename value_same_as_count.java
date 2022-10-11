2743a9b96604c252ceadcef841424562a695a913
21A91A1255 <97424886+21A91A1255@users.noreply.github.com>
2022-10-11 09:39:42 +0530

    import java.util.Scanner; class First{     public static void main(String args[])     {         int a,i,j,c=0,k=0;         Scanner sc= new Scanner(System.in);         a=sc.nextInt();         int[] arr = new int[100];        for(i=0;i<a;i++)        {            arr[i]=sc.nextInt();        }        for(i=0;i<a;i++)        {            c=1;            if(arr[i]==-1)            {                continue;            }            for(j=i+1;j<a;j++)            {                if(arr[i]==arr[j])                {                    c++;                    arr[j]=-1;                }            }            if(c==arr[i])            {                k++;            }        }        System.out.print(k);     } }

---
+++ value_same_as_count.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class First{
+    public static void main(String args[])
+    {
+        int a,i,j,c=0,k=0;
+        Scanner sc= new Scanner(System.in);
+        a=sc.nextInt();
+        int[] arr = new int[100];
+       for(i=0;i<a;i++)
+       {
+           arr[i]=sc.nextInt();
+       }
+       for(i=0;i<a;i++)
+       {
+           c=1;
+           if(arr[i]==-1)
+           {
+               continue;
+           }
+           for(j=i+1;j<a;j++)
+           {
+               if(arr[i]==arr[j])
+               {
+                   c++;
+                   arr[j]=-1;
+               }
+           }
+           if(c==arr[i])
+           {
+               k++;
+           }
+       }
+       System.out.print(k);
+    }
+}
