8d3c71a8274ad69d74f4ae4507f9cda0049980f1
21A91A1255 <97424886+21A91A1255@users.noreply.github.com>
2022-10-11 09:59:51 +0530

    import java.util.Scanner; class First{     public static void main(String args[])     {         int a,b,d,i,j,c,k=0,s=0,v=0;         Scanner sc= new Scanner(System.in);         a=sc.nextInt();         int[] arr = new int[100];        for(i=0;i<a;i++)        {            arr[i]=sc.nextInt();        }        for(i=0;i<a;i++)        {            if(arr[i]%2==0)            {                System.out.print(arr[i]+" ");            }        }        for(i=0;i<a;i++)        {            if(arr[i]%2!=0)            {                System.out.print(arr[i]+" ");            }        }     } }

---
+++ even_elements_followed_by_odd.java
@@ -0,0 +1,30 @@
+
+
+import java.util.Scanner;
+class First{
+    public static void main(String args[])
+    {
+        int a,b,d,i,j,c,k=0,s=0,v=0;
+        Scanner sc= new Scanner(System.in);
+        a=sc.nextInt();
+        int[] arr = new int[100];
+       for(i=0;i<a;i++)
+       {
+           arr[i]=sc.nextInt();
+       }
+       for(i=0;i<a;i++)
+       {
+           if(arr[i]%2==0)
+           {
+               System.out.print(arr[i]+" ");
+           }
+       }
+       for(i=0;i<a;i++)
+       {
+           if(arr[i]%2!=0)
+           {
+               System.out.print(arr[i]+" ");
+           }
+       }
+    }
+}
