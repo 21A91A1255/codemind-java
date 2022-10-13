9c9736c66864d857edfb15e240886fbadd47e9ae
21A91A1255 <97424886+21A91A1255@users.noreply.github.com>
2022-10-13 18:59:10 +0530

    import java.util.Scanner; import java.lang.Math; class First{     public static void main(String args[])     {         int p,a;         Scanner sc=new Scanner(System.in);         p=sc.nextInt();         a=p*(p-3)/2;         System.out.print(a);     } }

---
+++ Diagonals_of_polygon.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.lang.Math;
+class First{
+    public static void main(String args[])
+    {
+        int p,a;
+        Scanner sc=new Scanner(System.in);
+        p=sc.nextInt();
+        a=p*(p-3)/2;
+        System.out.print(a);
+    }
+}
