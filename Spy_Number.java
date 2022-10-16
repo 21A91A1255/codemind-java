40d7f4167e5e2e338637a46ed3786ce3f800da2d
21A91A1255 <97424886+21A91A1255@users.noreply.github.com>
2022-10-16 11:58:44 +0530

    import java.util.Scanner; import java.lang.Math; class First{     public static void main(String args[])     {         int a,b,c,n,r,s=0,p=1,t;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         while(n>0)         {             r=n%10;             s=s+r;             p=p*r;             n=n/10;         }         if(s==p)         {             System.out.print("Spy Number");         }         else         {             System.out.print("Not Spy Number");         } } }

---
+++ Spy_Number.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.lang.Math;
+class First{
+    public static void main(String args[])
+    {
+        int a,b,c,n,r,s=0,p=1,t;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        while(n>0)
+        {
+            r=n%10;
+            s=s+r;
+            p=p*r;
+            n=n/10;
+        }
+        if(s==p)
+        {
+            System.out.print("Spy Number");
+        }
+        else
+        {
+            System.out.print("Not Spy Number");
+        }
+}
+}
